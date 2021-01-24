0a7ae6fb732d6034ace503b7fb3d082b1f252657
Lukas Veneziano <code@venezilu.de>
2021-01-24 14:23:57 +0100

    syntactical sugar for version compare [ci skip]

--- ci/isNewRelease.sh
+++ ci/isNewRelease.sh
@@ -7,7 +7,8 @@ VAR1=$(cat modinfo.json | jq -r '.["version"]')
 VAR2=$(curl -LsS "${REPO}/raw/main/modinfo.json" | jq -r '.["version"]')
 
 vercomp $VAR1 $VAR2
-if [[ $? == 2 ]]; then
+vercomp_status=$?
+if [[ vercomp_status -eq 2 ]]; then
   echo "Version string was not changed: $VAR2 -> $VAR1"
   exit 1
 else
