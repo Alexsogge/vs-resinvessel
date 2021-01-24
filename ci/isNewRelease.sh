81ec571229ee66c251b2e52bda8a59f98a7e1931
Lukas Veneziano <code@venezilu.de>
2021-01-24 13:24:59 +0100

    more output for version compare

--- ci/isNewRelease.sh
+++ ci/isNewRelease.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 REPO=$(cat modinfo.json | jq -r '.["website"]')
+echo "Extracted repo url: $REPO"
 VAR1=$(cat modinfo.json | jq -r '.["version"]')
+echo "Version in this commit: $VAR1" 
 VAR2=$(curl ${REPO}/raw/main/modinfo.json | jq -r '.["version"]')
+echo "Version on main in repo: $VAR2" 
 
 if [[ "$VAR1" == "$VAR2" ]]; then
     echo "Version string was not changed: $VAR1, $VAR2"
